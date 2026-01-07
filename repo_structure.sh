eeda04b88020d0e29aad3a0b69a872c878e2e393
afrabouzehar <bouzehar.afra@gmail.com>
2026-01-07 16:18:35 +0100

    adding files and folders

---
+++ repo_structure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir 01_variables
+touch 01_variables/temperature_storage.c  01_variables/swap_variables.c  01_variables/ascii_values.c
+mkdir 02_input_output
+touch 02_input_output/read_two_numbers.c  02_input_output/read_full_name.c  02_input_output/read_char_getchar.c
+mkdir 03_conditions
+touch 03_conditions/compare_two_numbers.c 03_conditions/grade_checker.c 03_conditions/simple_menu.c
+mkdir 04_loops
+touch 04_loops/countdown.c 04_loops/sum_until_zero.c 04_loops/print_pattern.c
+mkdir 05_functions
+touch 05_functions/max_of_two.c 05_functions/repeat_message.c 05_functions/minutes_to_hours.c
+mkdir 06_arrays
+touch 06_arrays/find_max.c 06_arrays/count_even.c 06_arrays/reverse_print.c
+mkdir 07_strings
+touch 07_strings/count_vowels.c  07_strings/copy_string.c  07_strings/compare_strings.c
+mkdir 08_pointers
+touch 08_pointers/modify_through_pointer.c  08_pointers/pointer_array_print.c  08_pointers/swap_using_pointers.c
+mkdir 09_memory
+touch 09_memory/dynamic_input.c  09_memory/resize_memory.c  09_memory/free_memory.c
+mkdir projects
+mkdir projects/login_system projects/contact_card projects/file_writer projects/file_reader
+touch projects/login_system/login.c
+touch projects/contact_card/contact.c
+touch projects/file_writer/file_writer.c
+touch projects/file_reader/read_file.c
+
